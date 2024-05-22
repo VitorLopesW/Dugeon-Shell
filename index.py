cbeafdd58bb13ff28b4609fb17f8dcd8c2969325
VitorLopesW <67442259+VitorLopesW@users.noreply.github.com>
2024-05-22 15:05:03 -0300

    add miscellaneous.py

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
+import os
+
 from hero_class import Warrior, Wizard, Rogue
 from enemy import Goblin
 
 player = Warrior('Guts')
 
 moGoblin = Goblin()
-
-print(player.name)
-print(player.strength)
-print(moGoblin.strength)
+print('hello')
